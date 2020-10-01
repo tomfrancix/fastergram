0c56585f68a1ead33b4c66f75d415575da36587f
tomfrancix <58977284+tomfrancix@users.noreply.github.com>
2020-10-01 18:56:10 +0100

    Brand alteration

--- includes/navigation.php
+++ includes/navigation.php
@@ -7,7 +7,7 @@
                     <span class="glyphicon glyphicon-camera" style="font-size:15pt; color:lightgrey;"></span>
                     
                 </button>
-                <a class="navbar-brand" href="index.php">Instagram</a>
+                <a class="navbar-brand" href="index.php">Vortex</a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
